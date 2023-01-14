64dd395e067dfbf174dc6af07aebbf116adf31bb
mattegertondev <mattegertondev@gmail.com>
2023-01-14 12:32:35 +0000

    add audio with fn to initialise, count and end timer

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const timeout = 20;
 
+const tick = new Audio("./201766__waveplaysfx__tick.wav");
+
+const beep = new Audio("./96640__cgeffex__sci-fi-beep-03.wav");
+
+const timeRemainingDisplay = document.getElementById("time-remaining");
+
+function setTimeRemaining(timeRemaining) {
+  timeRemainingDisplay.innerHTML = timeRemaining;
+}
+
+function clearTimeRemaining() {
+  timeRemainingDisplay.style.display = "none";
+}
+
+function showTimeRemaining() {
+  timeRemainingDisplay.style.display = "block";
+}
+
 function startCountdown(seconds) {
-  console.log(seconds);
-  let timeRemaining = seconds;
-  const countdownTimer = setInterval(function () {
-    if (timeRemaining <= 0) {
-      clearInterval(countdownTimer);
-      timeRemaining = seconds;
-    }
-    timeRemaining -= 1;
-    console.log(timeRemaining);
+  beep.play();
+  setTimeout(() => {
+    showTimeRemaining();
+    let timeRemaining = seconds;
+    document.title = timeRemaining;
+    const countdownTimer = setInterval(function () {
+      setTimeRemaining(timeRemaining);
+      if (timeRemaining <= 0) {
+        clearInterval(countdownTimer);
+        clearTimeRemaining();
+        setTimeout(() => {
+          beep.play();
+        }, 1000);
+      }
+      tick.play();
+      timeRemaining -= 1;
+    }, 1000);
   }, 1000);
 }
-const interval = setInterval(startCountdown, 20000, timeout);
+const interval = setInterval(startCountdown, 50000, timeout);
